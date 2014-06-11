19191c0a33d4c3dae88d034e6896599c9c743d5f
Techokami <techokami@holenet.info>
2014-06-11 12:14:44 -0400

    Crafting recipe!
    Using a wonder material named "Prismatic Paste" (made from
    red/green/blue dyes and glowstone dust), adding some glass panes and a
    circuit board makes an OpenLight block!

--- src/main/java/pcl/openlights/OpenLights.java
+++ src/main/java/pcl/openlights/OpenLights.java
@@ -3,10 +3,15 @@ package pcl.openlights;
 import java.net.URL;
 import java.util.logging.Logger;
 
+import net.minecraft.init.Blocks;
 import pcl.openlights.blocks.LightBlock;
+import pcl.openlights.items.PrismaticPaste;
 import pcl.openlights.tileentity.OpenLightTE;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
@@ -17,6 +22,7 @@ import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
@@ -45,6 +51,8 @@ public class OpenLights {
 	
 	final Block openLightBlock = new LightBlock();
 	
+	public static Item  prismaticPaste;
+	
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
     	cfg = new Config(new Configuration(event.getSuggestedConfigurationFile()));
@@ -70,6 +78,28 @@ public class OpenLights {
         
     	GameRegistry.registerTileEntity(OpenLightTE.class, "OpenLightTE");
     	//openLightBlock.setCreativeTab(li.cil.oc.api.CreativeTab.instance);
-        
+		
+		prismaticPaste = new PrismaticPaste();
+		GameRegistry.registerItem(prismaticPaste, "openlights.prismaticPaste");
+    }
+
+    @EventHandler
+    public void load(FMLInitializationEvent event)
+    {
+        ItemStack redDye	    = new ItemStack(Items.dye, 1, 1);
+        ItemStack greenDye	    = new ItemStack(Items.dye, 1, 2);
+        ItemStack blueDye	    = new ItemStack(Items.dye, 1, 4);
+        ItemStack glowDust      = new ItemStack(Items.glowstone_dust);
+        ItemStack pcb		    = li.cil.oc.api.Items.get("printedCircuitBoard").createItemStack(1);
+        ItemStack glassPane     = new ItemStack(Blocks.glass_pane);
+
+        GameRegistry.addShapelessRecipe( new ItemStack(prismaticPaste, 4), redDye, greenDye, blueDye, glowDust);
+
+        GameRegistry.addRecipe( new ItemStack(openLightBlock, 1),
+                " G ",
+                "GPG",
+                " C ",
+                'G', glassPane, 'P', prismaticPaste, 'C', pcb);
+
     }
 }
